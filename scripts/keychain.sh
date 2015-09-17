cf7bb6a9ad913d8027326c01b28e34dfbcc4cf3e
satorupan <satorupan@icloud.com>
2015-09-17 17:52:54 +0900

    QQQ

--- scripts/keychain.sh
+++ scripts/keychain.sh
@@ -9,8 +9,8 @@ echo $APPLE_CERTIFICATE | base64 -D > $DIR/apple.cer
 echo $DISTRIBUTION_P12KEY | base64 -D > $DIR/dist.p12
 echo $DISTRIBUTION_CERTIFICATE | base64 -D > $DIR/dist.cer
 security create-keychain -p "$KEYCHAIN_PASSWORD" ios-build.keychain
-security import $DIR/apple.cer -k $KEYCHAIN -T /usr/bin/codesign
-#security import $DIR/dist.cer  -k $KEYCHAIN -T /usr/bin/codesign
+#security import $DIR/apple.cer -k $KEYCHAIN -T /usr/bin/codesign
+security import $DIR/dist.cer  -k $KEYCHAIN -T /usr/bin/codesign
 #security import $DIR/dist.p12  -k $KEYCHAIN -T /usr/bin/codesign -P "$P12KEY_PASSWORD"
 security list-keychain -s $KEYCHAIN
 security unlock-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN
