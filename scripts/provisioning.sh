4c61408fe1b6007eadc589c922aaa68f28df6a13
satorupan <satorupan@icloud.com>
2015-09-17 20:19:47 +0900

    Q

--- scripts/provisioning.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-bundle exec ios profiles:download:all --type distribution -u "$ITUNES_CONNECT_ACCOUNT" -p "$ITUNES_CONNECT_PASSWORD" >/dev/null 2>&1
-mkdir MobileProvisionings
-mv *.mobileprovision MobileProvisionings
