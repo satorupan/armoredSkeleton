44fdb4060ca3c4abf6edc20f605d39d4dcfdc679
satorupan <satorupan@icloud.com>
2015-09-17 17:54:13 +0900

    HHHHH

--- scripts/keychain.sh
+++ scripts/keychain.sh
@@ -5,11 +5,11 @@ KEYCHAIN=$HOME/Library/Keychains/ios-build.keychain
 KEYCHAIN_PASSWORD=`openssl rand -base64 48`
 rm -rf $DIR
 mkdir -p $DIR
-echo $APPLE_CERTIFICATE | base64 -D > $DIR/apple.cer
+echo $APPLE_AUTHORITY_BASE64 | base64 -D > $DIR/apple.cer
 echo $DISTRIBUTION_P12KEY | base64 -D > $DIR/dist.p12
 echo $DISTRIBUTION_CERTIFICATE | base64 -D > $DIR/dist.cer
 security create-keychain -p "$KEYCHAIN_PASSWORD" ios-build.keychain
-#security import $DIR/apple.cer -k $KEYCHAIN -T /usr/bin/codesign
+security import $DIR/apple.cer -k $KEYCHAIN -T /usr/bin/codesign
 security import $DIR/dist.cer  -k $KEYCHAIN -T /usr/bin/codesign
 #security import $DIR/dist.p12  -k $KEYCHAIN -T /usr/bin/codesign -P "$P12KEY_PASSWORD"
 security list-keychain -s $KEYCHAIN
