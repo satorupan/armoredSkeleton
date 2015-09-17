0b6b87a8ab43819be1cb8a07897add98a95ecd2b
satorupan <satorupan@icloud.com>
2015-09-17 17:33:34 +0900

    TT

--- scripts/keychain.sh
+++ scripts/keychain.sh
@@ -10,15 +10,15 @@ echo $DISTRIBUTION_P12KEY | base64 -D > $DIR/dist.p12
 echo $DISTRIBUTION_CERTIFICATE | base64 -D > $DIR/dist.cer
 security create-keychain -p "$KEYCHAIN_PASSWORD" ios-build.keychain
 security import $DIR/apple.cer -k $KEYCHAIN -T /usr/bin/codesign
-security import $DIR/dist.cer  -k $KEYCHAIN -T /usr/bin/codesign
-security import $DIR/dist.p12  -k $KEYCHAIN -T /usr/bin/codesign -P "$P12KEY_PASSWORD"
+#security import $DIR/dist.cer  -k $KEYCHAIN -T /usr/bin/codesign
+#security import $DIR/dist.p12  -k $KEYCHAIN -T /usr/bin/codesign -P "$P12KEY_PASSWORD"
 security list-keychain -s $KEYCHAIN
 security unlock-keychain -p "$KEYCHAIN_PASSWORD" $KEYCHAIN
 rm -rf $DIR
 
 PROVISIONING=~/Library/MobileDevice/Provisioning\ Profiles
 mkdir -p "$PROVISIONING"
-ios profiles:download:all --type distribution -u $APPLE_DEVELOPER_ID -p $APPLE_DEVELOPER_PASSWORD --team "$DISTRIBUTION_NAME"
+bundle exec ios profiles:download:all --type distribution -u $APPLE_DEVELOPER_ID -p $APPLE_DEVELOPER_PASSWORD --team "$DISTRIBUTION_NAME"
 echo "./App.mobileprovision -> $PROVISIONING_PROFILE.mobileprovision"
 mv ./App.mobileprovision "$PROVISIONING/$PROVISIONING_PROFILE.mobileprovision"
 rm -rf *.mobileprovision
